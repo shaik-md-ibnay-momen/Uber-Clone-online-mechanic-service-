42d99d4dd6041137903362c069a985939299db5f
wolverine97 <41838629+wolverine97@users.noreply.github.com>
2019-11-27 17:22:22 +0600

    Update google_maps_requests.dart

--- lib/google_maps_requests.dart
+++ lib/google_maps_requests.dart
@@ -1,7 +1,7 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-const apiKey = "AIzaSyBhrISC6WhEbIItMqafeBPPx3xqI3ru-n0";
+const apiKey = "your API";
 
 class GoogleMapsServices{
   Future<String> getRouteCoordinates(LatLng l1, LatLng l2)async{
